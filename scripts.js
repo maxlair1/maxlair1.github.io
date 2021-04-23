b480eefa74338e75c615ae13cbb09c791781bc96
maxlair1 <max@lairfamily.com>
2021-04-23 18:17:37 -0400

    Changed header font and button style

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
 $(window).load(function() {
     $("body").removeClass("preload");
   })
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
